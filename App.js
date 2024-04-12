71aa794e342996947206b0d8bff367c11e06a5d5
rcambiaghi <81322972+rcambiaghi@users.noreply.github.com>
2024-04-13 00:45:03 +0200

    Setup
    Config Commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import Signup from './src/pages/Signup'
+import Login from './src/pages/Login'
+import Registration from './src/pages/Registration'
+import TodoListScreen from './src/pages/TodoListScreen';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
+        <Stack.Screen name="Signup" component={Signup} options={{headerShown: false}} />
+        <Stack.Screen name="Registration" component={Registration} options={{headerShown: false}} />
+        <Stack.Screen name="TodoList" component={TodoListScreen} options={{headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
